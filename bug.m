b20cb70b7e1e0ed106f818e85892efd2dcdae9c2
BugHunter-X <askzenai@gmail.com>
2024-12-04 22:00:07 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,12 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  if input < 0
+    error('Input must be non-negative');
+  end
+  result = someCalculation(input);
+end
+
+function output = someCalculation(x)
+  % This function might throw an error if x is too large
+  output = x^2; 
+end
